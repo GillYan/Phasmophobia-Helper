a0a9207a310a335e461e8c3e705db6151b01dfc6
Yan <xGillYan@gmail.com>
2020-11-12 11:26:10 -0500

    Changed checkboxes to buttons

--- static/js/init.js
+++ static/js/init.js
@@ -1,17 +1,25 @@
 $(document).ready(function () {
-	var inputText;
-
 	console.log("Gillian Sanchez, 1003184")
 
+	//function to get the evidences that have been checked
 	function update() {
 		var checkboxValues = [];
         $('input[type="checkbox"]:checked').each(function(index, elem) {
             checkboxValues.push($(elem).val());
         });
-        $('#debugOutput').html(checkboxValues.join(','));
+
+        //check if there any checkboxes that are checked so that elements don't keep moving up and down the page
+        if (checkboxValues === undefined || checkboxValues.length == 0) {
+        	//set to a zero width space
+        	document.getElementById("debugOutput").innerHTML = "&nbsp";
+        }
+        else {
+        	document.getElementById("debugOutput").innerHTML = checkboxValues.join(',');
+        }
 	}
 
 
+	//"event listeners"
 	$("#EMF5").click(update);
 	$("#SpiritBox").click(update);
 	$("#Fingerprints").click(update);
